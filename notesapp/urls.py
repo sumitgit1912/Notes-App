cb17b3bfd8a0cf22034484e3f6913238ef392e54
sumitgit1912 <sumitandariya@gmail.com>
2025-07-18 01:39:39 +0530

    add changes in routes

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from  .views import *
 
 urlpatterns = [
-    path('', NotesListView.as_view(), name='notes'),
+    path('notes/', NotesListView.as_view(), name='notes'),
     path('signup/', SignupView.as_view(), name='signup'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('', CustomLoginView.as_view(), name='login'),
     path('note/new/', NoteCreateView.as_view(), name='create_note'),
     path('note/<int:pk>/delete/', NoteDeleteView.as_view(), name='delete_note'),
     path('note/<int:pk>/update/', NoteUpdateView.as_view(), name='update_note'),
