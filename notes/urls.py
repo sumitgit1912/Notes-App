57e1007c29e34da3c7bf45ac7bc5245771c3e77b
sumitgit1912 <sumitandariya@gmail.com>
2025-07-18 01:29:55 +0530

    Revert "Redirect root URL to login related changes"
    This reverts commit 776ec6324c26fccaffda63793573704873569f5b.

--- notes/urls.py
+++ notes/urls.py
@@ -1,12 +1,8 @@
+
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
-
-def base_redirect(request):
-    return redirect('login')
 
 urlpatterns = [
-    path('', base_redirect),  # Redirect root URL to login
-    path('notes/', include('notesapp.urls')),  # Prefix all app URLs with /notes/
     path('admin/', admin.site.urls),
+    path('', include('notesapp.urls'))
 ]
