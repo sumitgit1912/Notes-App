41407fb5cb9533a52615f138f099ccbb30adbaee
sumitgit1912 <sumitandariya@gmail.com>
2025-07-29 23:16:52 +0530

    Updated admin panel configurations

--- notesapp/admin.py
+++ notesapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at', 'updated_at')
+    list_filter = ('user', 'created_at')
+    search_fields = ('title', 'content', 'user__username')
+    ordering = ('-created_at',)
+    
+admin.site.register(Note, NoteAdmin)
